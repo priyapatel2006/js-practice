83dd9228600640502743b74517a6c643ee240076
Priya Patel <priyapatel2k6@gmail.com>
2025-12-19 15:51:57 +0530

    Task-1,2and 3 completed.

---
+++ app.js
@@ -0,0 +1,35 @@
+// var a = "45";
+
+// {
+//     var a = "40";
+//     console.log(a);
+// }
+
+// console.log(a);
+
+// function test() {
+//     var x = 10;
+//     console.log(x);
+// }
+
+// test();
+// console.log(x); // ❌ error
+
+// Task 1
+// var name = "pratik";
+// name = "Rahul";
+// console.log(name);
+
+// Task 2
+// const city = "Pune";
+// city = "Mumbai"
+// console.log(city);
+
+// Task 3
+// let person = {
+//     name: "Pratik",
+//     age: 24
+// }
+// person.age = 25;
+
+// console.log(person);
