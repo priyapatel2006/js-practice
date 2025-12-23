71d33cb309ba3621978a732612360e1a84e2bd86
priya <priyapatel2k6@gmail.com>
2025-12-23 12:36:30 +0530

    Completed day4.

---
+++ Day_4.js
@@ -0,0 +1,66 @@
+// const mySym = Symbol('key1')
+// const user = {
+//     name: 'Priya',
+//     'Full name': 'Priya Patel',
+//     [mySym]: 'mykey1',
+//     age: 20,
+//     location: 'pune',
+//     email: 'priyapatel2k6',
+//     isLoggedIn: false,
+//     lastlogin: ['Monday', 'saturday']
+// }
+// console.log(user.email);
+// console.log(user['full name']);
+// console.log(user[mySym]);
+// user.email = 'priyapratik@2k6'
+// Object.freeze(user)
+// user.email = 'priyu@2k6'
+
+// console.log(user);
+// user.greeting = function () {
+//     console.log('hello js user');
+// }
+// user.greetingTwo = function () {
+//     console.log(`hello js user,${this.name}`);
+// }
+// console.log(user.greeting());
+// console.log(user.greetingTwo());
+
+//creating objects accesing prpertise
+
+// const whoAmI = {
+//     Myname: 'Priya Patel',
+//     age: 20,
+//     profession: 'student',
+//     hobbies: ['reading', 'coding', 'traveling'],
+//     location: {
+//         city: 'Pune',
+//         pincode: 110001,
+//     }
+// }
+// console.log(whoAmI.location.city);
+
+// const user = {
+//     name: "Priyu",
+//     age: 22
+// };
+// const {name,age}=user;
+// console.log(name,age);
+
+//Task1  Read Property: Access object value
+
+
+// const Babu = {
+//     name: "Pratik",
+//     role: "Developer"
+// }
+// console.log(Babu.role);
+
+// Task 2 – Destructuring: Extract values from object
+// const user = { city: "Pune", country: "India" }
+// const allData = { city, country } = user;
+// console.log(city, country);
+
+// Task 3 – Nested Object: Access nested property
+// const person = { user: { name: "Rahul" } }
+// console.log(person.user.name);
