cba4a08b754fe9c4ad8c72c5cb0bc5cc69f9fba3
priya <priyapatel2k6@gmail.com>
2025-12-24 17:34:03 +0530

    completed day 7.

---
+++ Day_7.js
@@ -0,0 +1,8 @@
+// Day 7 – Week 1 Project
+// Project: User Management Console App
+// const users = [
+//     { name: "A", active: true },
+//     { name: "B", active: false }
+// ]
+// let filterUsers = users.filter(item => item.active)
+// console.log(filterUsers)
