e0a9505fd474b651700eacaefca8d4249a73c9ce
priya <priyapatel2k6@gmail.com>
2025-12-24 17:10:23 +0530

    day 6 is comlpeted.

---
+++ Day_6.js
@@ -0,0 +1,117 @@
+//Day 6 – Conditional Logic & Loops
+
+let age = 0;
+// if (age >= 18) {
+//     console.log('he can drive')
+// } else {
+//     console.log('he cannot drive')
+// }
+// if (age== 18) {
+//     console.log('you can drive')
+// } else if (age == 0) {
+//     console.log('are you kidding?')
+// } else {
+//     console.log('you can not drive');
+// }
+
+// const cups = 4;
+// switch (cups) {
+//     case 4:
+//         console.log('the value of cup is 4');
+//         break;
+//     case 41:
+//         console.log('the value of cup is 41');
+//         break;
+//     case 42:
+//         console.log('the value of cup is 42');
+//         break;
+//     case 43:
+//         console.log('the value of cup is 43');
+//         break;
+
+//     default:
+//         console.log('the value of cup is none of 4, 41,42,43');
+//         break;
+// }
+
+// const Days = 4;
+// switch (Days) {
+//     case 1:
+//         console.log('Monday');
+//         break;
+//     case 2:
+//         console.log('Tuesday');
+//         break;
+//     case 3:
+//         console.log('Wednesday');
+//         break;
+//     case 4:
+//         console.log('Thursday');
+//         break;
+
+//     default:
+//         console.log('Invalid day');
+//         break;
+// }
+
+// let sum = 0;
+// let n = 4;
+// n = Number.parseInt(n)
+// for (let i = 0; i < n; i++) {
+//     sum += (i + 1)
+// }
+// console.log('sum of first ' + n + ' natural numbers is ' + sum);
+
+// let obj = {
+//     priya: 90,
+//     ravi: 85,
+//     aman: 78,
+//     sonam: 88
+// }
+// for (let a in obj) {
+//     console.log('marks of  ' + a + ' are ' + obj[a]);
+
+// }
+
+// let a = 30;
+// let b;
+// b = "Value is " + (a == 20 ? z = 'true' : z = 'false')
+// console.log(b);
+
+
+// let number = 4;
+
+// let result = number % 2 === 0 ? "Even" : "Odd";
+
+// console.log("Your number is " + result);
+
+
+// 1. **Task 1 – Even or Odd:** Check number type
+
+// let number = 4;
+// let result = number % 2 === 0 ? 'Even' : 'Odd'
+// console.log("Your number is  " + result);
+
+// Task 2 – Login Check: Validate login state
+
+// let isLoggedIn = false;
+// isLoggedIn = (isLoggedIn === true) ? "LoggedIn" : "Please Login"
+// console.log(isLoggedIn)
+
+// Task 3 – Grade System: Calculate grade
+// let grade = 90;
+// grade = (grade >= 85) ? 'A Grade' : 'B Grade'
+// console.log(grade)
+
+// let student = 60;
+// if (student >= 85) {
+//     console.log('A Grade')
+// } else if (student >= 60) {
+//     console.log('B Grade')
+// } else {
+//     console.log('C Grade')
+// }
+
+
+
+
